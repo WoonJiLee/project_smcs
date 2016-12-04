4880e43d8a0224f3590dc66ac1985fcadbef1227
WoonJiLee <wj53609@gamil.com>
2016-12-04 23:22:48 +0900

    phonedelete

--- src/Delete_Phone.java
+++ src/Delete_Phone.java
@@ -1,15 +1,28 @@
 import java.awt.EventQueue;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Delete_Phone {
 
 	private JFrame frame;
 	private JTextField textField;
 	private String deletename;
+	Connection con = null;
+	PreparedStatement ps = null;
+	ResultSet rs = null;
+	String url = "jdbc:mysql://127.0.0.1:3306/SoftwareEnginnerHw";
+	String user = "root";
+	String pass = "4175^^";
 
 	/**
 	 * Launch the application.
@@ -38,6 +51,12 @@ public class Delete_Phone {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		try {
+			con = DriverManager.getConnection(url,user,pass);
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		frame = new JFrame("삭제하기");
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -54,10 +73,32 @@ public class Delete_Phone {
 		deletename = textField.getText();
 		
 		JButton btnNewButton = new JButton("삭제");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try{          
+					
+					String sql = "DELETE FROM Phonebook WHERE name="+deletename;        
+					ps = con.prepareStatement(sql); 
+					ps.executeUpdate();
+					int n = ps.executeUpdate();
+					if(n>0){
+						System.out.println("추가 성공");
+					}else{
+						System.out.println("추가 실패");
+					}
+				}catch(SQLException e1){
+					e1.printStackTrace();
+				}
+			}
+		});
 		btnNewButton.setBounds(78, 129, 105, 27);
 		frame.getContentPane().add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("취소");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		btnNewButton_1.setBounds(217, 129, 105, 27);
 		frame.getContentPane().add(btnNewButton_1);
 	}
