98070cbca5268962a2ecca2f742f6f5dce1a5c8d
WoonJiLee <wj53609@gamil.com>
2016-12-06 23:50:54 +0900

    interface추가

---
+++ src/ViewAddDeleteInterface.java
@@ -0,0 +1,14 @@
+
+public interface ViewAddDeleteInterface {
+	
+	public String getId();
+	
+	public void setId(String userId);
+	
+	public void showTable();
+	
+	public void addValue();
+	
+	public void deleteValue();
+
+}
